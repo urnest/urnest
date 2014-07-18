6e1558b35b1d042d485e0881d0379651e88f092a
Trevor Taylor <urnest@onthenet.com.au>
2014-07-18 23:43:29 +1000

    add test case for class via template specialisation

---
+++ hcp/test/classpecialize/b.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2014 Trevor Taylor
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all.
+// Trevor Taylor makes no representations about the suitability of this
+// software for any purpose.  It is provided "as is" without express or
+// implied warranty.
+//
+#ifndef HCP_TEST_CLASSPECIALIZE_B_H
+#define HCP_TEST_CLASSPECIALIZE_B_H
+
+#include "x.hh"
+
+int b()
+{
+  return X<int>().f();
+}
+
+#endif
+
---
+++ hcp/test/classpecialize/main.cc
@@ -0,0 +1,7 @@
+#include "a.h"
+#include "b.h"
+
+int main(int argc, char* argv[])
+{
+  return a()-b();
+}
