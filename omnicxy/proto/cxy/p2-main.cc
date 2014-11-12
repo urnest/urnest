bfb81ea590b969da81dd35b48da1a2af8221fc80
Trevor Taylor <urnest@onthenet.com.au>
2014-11-12 22:33:19 +1000

    draft p2*.hcp

---
+++ omnicxy/proto/cxy/p2-main.cc
@@ -0,0 +1,103 @@
+// Copyright (c) 2014 Trevor Taylor
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all.
+// Trevor Taylor makes no representations about the suitability of this
+// software for any purpose.  It is provided "as is" without express or
+// implied warranty.
+//
+
+
+#include "p2.hh"
+#include "p2.cref.hh"
+//#include "p2.sref.hh"
+
+#include <xju/Exception.hh>
+#include <iostream>
+#include <string>
+#include <xju/format.hh>
+#include <xju/stringToInt.hh>
+#include <stdlib.h>
+#include <xju/mt.hh>
+#include <xju/Time.hh>
+#include <cxy/ORB.hh>
+
+std::string makeURI(int port, std::string const& objectName) throw()
+{
+  std::ostringstream s;
+  s << "corbaloc:iiop:localhost:"<< port << "/" << objectName;
+  return s.str();
+}
+
+class F_impl : public p2::F
+{
+public:
+  ~F_impl() throw()
+  {
+  }
+  
+  virtual void f1() throw(cxy::Exception)
+  {
+    std::cout << "F::f1()" << std::endl;
+  }
+};
+
+  
+int main(int argc, char* argv[])
+{
+  try {
+    if (argc != 3 || !(std::string("client")==argv[2]||
+                       std::string("server")==argv[2]||
+                       std::string("both")==argv[2])) {
+      std::cerr << "usage:  " 
+                << argv[0] << " <ip-port> [client|server|both]" << std::endl;
+      return 1;
+    }
+    
+    std::string const OBJECT_NAME("p2");
+    
+    int const port(xju::stringToInt(argv[1]));
+    
+    if (argv[2]==std::string("client")) {
+      cxy::ORB<cxy::Exception> orb("giop:tcp::");
+      cxy::cref<p2::F> ref(orb, makeURI(port, OBJECT_NAME));
+      ref->f1();
+    }
+/*
+    else if (argv[2]==std::string("server")) {
+      std::string const orbEndPoint="giop:tcp::"+xju::format::str(port);
+      cxy::ORB<cxy::Exception> orb(orbEndPoint);
+
+      F_impl x;
+      
+      cxy::sref<p2::F> const xa(orb, OBJECT_NAME, x);
+      
+      orb.monitorUntil(xju::Time::now()+xju::MicroSeconds(30*1000000));
+    }
+    else
+    {
+      std::string const orbEndPoint="giop:tcp::"+xju::format::str(port);
+      cxy::ORB<cxy::Exception> orb(orbEndPoint);
+      F_impl x;
+      
+      cxy::sref<p2::F> const xa(orb, OBJECT_NAME, x);
+      
+      cxy::cref<p2::F> ref(orb, makeURI(port, OBJECT_NAME));
+      ref->f1();
+    }
+*/    
+    return 0;
+  }
+  catch(xju::Exception& e) {
+    e.addContext(xju::format::join(argv, argv+argc, " "), XJU_TRACED);
+    std::cerr << readableRepr(e) << std::endl;
+    return 1;
+  }
+  catch(cxy::Exception& e) {
+    e.addContext(xju::format::join(argv, argv+argc, " "), 
+                 std::make_pair(__FILE__, __LINE__));
+    std::cerr << readableRepr(e, true, false) << std::endl;
+    return 1;
+  }
+}
