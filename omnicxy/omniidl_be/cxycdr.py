1e0ffdbed8c8fa905bd4c0d2484fb54eedd64341
Trevor Taylor <urnest@onthenet.com.au>
2014-12-19 23:25:21 +1000

    e/p7-main

--- omnicxy/omniidl_be/cxycdr.py
+++ omnicxy/omniidl_be/cxycdr.py
@@ -94,33 +94,47 @@ public:
   static const char repoId[]="%(repoId)s";
 };
 '''
-def mapped_marshal(t,n,causeType,contextType):
+def mapped_marshal(t,n,
+                   causeType,
+                   contextType,
+                   causeMemberExpression,
+                   contextMemberExpression):
     if t==causeType:
-        return '\n    cdr< %(t)s>::marshal(%(t)s(x.%(n)s),s);'%vars()
+        causeMemberExpression=causeMemberExpression or n
+        return '\n    cdr< %(t)s>::marshal(%(t)s(x.%(causeMemberExpression)s),s);'%vars()
     elif t==contextType:
-        return '\n    cdr< %(t)s>::marshal(%(t)s(x.%(n)s.begin(),x.%(n)s.end()),s);'%vars()
+        contextMemberExpression=contextMemberExpression or n
+        return '\n    cdr< %(t)s>::marshal(%(t)s(x.%(contextMemberExpression)s.begin(),x.%(contextMemberExpression)s.end()),s);'%vars()
     return '\n    cdr< %(t)s>::marshal(x.%(n)s,s);'%vars() 
     
-def gen_mapped_exception(name,repoId,memberTypesAndNames,eclass,causeType,contextType):
+def gen_mapped_exception(name,repoId,memberTypesAndNames,
+                         eclass,causeType,contextType,
+                         causeMemberExpression,contextMemberExpression):
     memberNames=[_[1] for _ in memberTypesAndNames]
     memberTypes=[_[0] for _ in memberTypesAndNames]
     paramNames=['p%s'%i for i in range(1,len(memberTypesAndNames)+1)]
     memberUnmarshals=''.join(['\n    %(t)s const %(pn)s(cdr< %(t)s>::unmarshalFrom(s));'%vars() for t,pn in zip(memberTypes,paramNames)])
     consparams=','.join(paramNames)
-    memberMarshals=''.join([mapped_marshal(t,n,causeType,contextType) \
+    memberMarshals=''.join([mapped_marshal(t,n,
+                                           causeType,contextType,
+                                           causeMemberExpression,
+                                           contextMemberExpression) \
                                 for t,n in zip(memberTypes,memberNames)])
     return mapped_exception_t%vars()
 
-def gen(decl,eclass,eheader,causeType,contextType,indent=''):
+def gen(decl,eclass,eheader,causeType,contextType,
+        causeMemberExpression,contextMemberExpression,indent=''):
     result=''
     if isinstance(decl, idlast.Module):
-        result=''.join(gen(_,eclass,eheader,causeType,contextType) \
+        result=''.join(gen(_,eclass,eheader,causeType,contextType,
+                           causeMemberExpression,contextMemberExpression) \
                            for _ in decl.definitions())
     elif isinstance(decl, idlast.Interface):
         fqn='::'.join(decl.scopedName())
         repoId=decl.repoId()
         result=interface_t%vars()+\
-            ''.join(gen(_,eclass,eheader,causeType,contextType) \
+            ''.join(gen(_,eclass,eheader,causeType,contextType,
+                        causeMemberExpression,contextMemberExpression) \
                         for _ in decl.contents())
     elif isinstance(decl, idlast.Operation):
         pass
@@ -139,8 +153,10 @@ def gen(decl,eclass,eheader,causeType,contextType,indent=''):
                 for _ in decl.members()];
         memberTypes=[_[0] for _ in memberTypesAndNames]
         if causeType in memberTypes and contextType in memberTypes:
-            result=gen_mapped_exception(name,repoId,memberTypesAndNames,
-                                        eclass,causeType,contextType)
+            result=gen_mapped_exception(
+                name,repoId,memberTypesAndNames,
+                eclass,causeType,contextType,
+                causeMemberExpression,contextMemberExpression)
         else:
             result=gen_exception(name,repoId,memberTypesAndNames,eclass)
             pass
@@ -182,6 +198,14 @@ def run(tree, args):
     contextType=([_.split('-contextType=',1)[1] for _ in args \
                                 if _.startswith('-contextType')]+\
                                [None])[0]
+    causeMemberExpression=(
+        [_.split('-causeMemberExpression=',1)[1] for _ in args \
+             if _.startswith('-causeMemberExpression')]+\
+            [None])[0]
+    contextMemberExpression=(\
+        [_.split('-contextMemberExpression=',1)[1] for _ in args \
+             if _.startswith('-contextMemberExpression')]+\
+            [None])[0]
     if eheader.startswith('./') or os.path.dirname(eheader)=='':
         eheader='"%s"'%eheader[2:]
     else:
@@ -189,7 +213,8 @@ def run(tree, args):
     assert tree.file().endswith('.idl'), tree.file()
     fileName=os.path.basename(tree.file())
     baseName=fileName[0:-4]
-    items=''.join([gen(_,eclass,eheader,causeType,contextType) \
+    items=''.join([gen(_,eclass,eheader,causeType,contextType,
+                       causeMemberExpression,contextMemberExpression) \
                        for _ in tree.declarations() \
                        if _.mainFile()])
     idlincludes=gen_idlincludes(set([_.file() for _ in tree.declarations() if not _.mainFile()]))
