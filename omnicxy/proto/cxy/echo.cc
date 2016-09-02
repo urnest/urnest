66d6284fa7a2a56153c91021fc72ed0d9d478f19
Trevor Taylor <urnest@onthenet.com.au>
2016-09-02 19:45:32 +1000

    turn off source file and line tracking for echo.cc since we check it in

--- omnicxy/proto/cxy/echo.cc
+++ omnicxy/proto/cxy/echo.cc
@@ -1,9 +1,6 @@
 #include "echo.hh"
-#line 1 "/home/xju/tmp/oc52-2/xjutv/FILES/a/f/echo.idl.1176360.omnicxy.output/echo.hcp"
-#line 8
 #include <cxy/copyContext.hh> //impl
 
-#line 13
 echo::~echo() throw()
   {
   }
