e7da860f1b2806791e6f307cea70dfcdcd031b29
Trevor Taylor <urnest@onthenet.com.au>
2018-02-21 07:28:37 +1000

    add xju::eternity

---
+++ xju/test-eternity.cc
@@ -0,0 +1,33 @@
+// Copyright (c) 2017 Trevor Taylor
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all.
+// Trevor Taylor makes no representations about the suitability of this
+// software for any purpose.  It is provided "as is" without express or
+// implied warranty.
+//
+#include <xju/eternity.hh>
+
+#include <iostream>
+#include <xju/assert.hh>
+#include <xju/now.hh>
+
+namespace xju
+{
+
+void test1() {
+  xju::assert_less(xju::now(),xju::eternity());
+}
+
+}
+
+using namespace xju;
+
+int main(int argc, char* argv[])
+{
+  unsigned int n(0);
+  test1(), ++n;
+  std::cout << "PASS - " << n << " steps" << std::endl;
+  return 0;
+}
