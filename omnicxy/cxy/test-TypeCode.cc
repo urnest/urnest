b7202cd1e245b4127babc1fb2f769c0b42229620
Trevor Taylor <urnest@onthenet.com.au>
2018-07-04 22:54:46 +1000

    toward recursive TypeCode/Any support

--- omnicxy/cxy/test-TypeCode.cc
+++ omnicxy/cxy/test-TypeCode.cc
@@ -20,25 +20,81 @@ namespace cxy
 
 template<class T>
 void testx(T const v) {
-  cdrMemoryStream s;
+  {
+    cxy::MemCdrStream s;
+    cdr<T>::marshal(v,*s);
+    
+    xju::assert_equal(cdr<T>::unmarshalFrom(*s),v);
+  }
+  cxy::MemCdrStream s;
   ::cxy::TypeCode const x(cxy::createTypeCodeOf<T>());
-  x->marshal(s);
-  cdr<T>::marshal(v,s);
+  ::cxy::cdr< ::cxy::TypeCode>::marshal(x,*s);
+  cdr<T>::marshal(v,*s);
 
-  ::cxy::TypeCode y(cdr< ::cxy::TypeCode >::unmarshalFrom(s));
+  ::cxy::TypeCode y(cdr< ::cxy::TypeCode >::unmarshalFrom(*s));
   xju::assert_equal(*x,*y);
   
-  cdrMemoryStream t;
-  y->copyValue(s,t);
+  MemCdrStream t;
+  y->copyValue(*s,*t);
   
-  xju::assert_equal(cdr<T>::unmarshalFrom(t),v);
+  xju::assert_equal(cdr<T>::unmarshalFrom(*t),v);
 }
 
+class Tree
+{
+public:
+  Tree(std::string const& name,
+       std::vector<Tree> const& children) throw():
+      name_(name),
+      children_(children)
+  {
+  };
+  std::string name_;
+  std::vector<Tree> children_;
+  friend bool operator==(Tree const& a, Tree const& b) throw(){
+    return (a.name_ == b.name_) &&
+      (a.children_==b.children_);
+  }
+};
+template<>
+class cdr<Tree>
+{
+public:
+  static void marshal(Tree const& x, cdrStream& s)
+  {
+    ::cxy::cdr< std::string >::marshal(x.name_,s);
+    ::cxy::cdr< std::vector<Tree> >::marshal(x.children_,s);
+  }
+  static Tree unmarshalFrom(cdrStream& s)
+  {
+    auto const name(::cxy::cdr< std::string >::unmarshalFrom(s));
+    auto const children(
+      ::cxy::cdr< std::vector<Tree> >::unmarshalFrom(s));
+    return Tree(name,children);
+  }
+};
+template<>
+class TypeCodeOf<Tree>
+{
+public:
+  static TypeCode create(cxy::TypeCodeRefIndex& index) throw(){
+    TypeCode name(cxy::createTypeCodeOf< std::string >(index));
+    TypeCode children(
+      cxy::createTypeCodeOf< std::vector<Tree> >(index));
+    return TypeCode(std::shared_ptr<TypeCode_>(
+                      new cxy::StructTypeCode(
+                        "Tree",
+                        "Tree",
+                        { {"name",name},
+                          {"children",children} })));
+  }
+};
 void test1()
 {
   testx((int16_t)27);
   testx((int32_t)-32);
   testx(std::vector<int16_t>{3});
+  testx(Tree("fred",{Tree("jock",{})}));
 }
 
 }
