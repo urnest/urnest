e617dad4e6ffde4e15afc2b3b88031d62c8c2109
Trevor Taylor <urnest@onthenet.com.au>
2018-08-31 20:22:23 +1000

    parse array

--- xju/json/test-parse.cc
+++ xju/json/test-parse.cc
@@ -54,6 +54,44 @@ void test1() {
   catch(xju::Exception const& e){
     xju::assert_equal(readableRepr(e),"Failed to get 1.06e10 (at line 1 column 1) as a bool because\n1.06e10 (at line 1 column 1) is not true or false.");
   }
+
+  {
+    std::string const s{" [ 1,2 , 3] "};
+    auto const x{parse(Utf8String(s))};
+    auto const y{x->asArray()};
+    xju::assert_equal(y.size(),3);
+    xju::assert_equal(y[0]->asDouble(),1.0);
+    xju::assert_equal(y[1]->asDouble(),2.0);
+    xju::assert_equal(y[2]->asDouble(),3.0);
+  }
+  {
+    std::string const s{" [ ] "};
+    auto const x{parse(Utf8String(s))};
+    auto const y{x->asArray()};
+    xju::assert_equal(y.size(),0U);
+  }
+  try{
+    std::string const s{" [ 3 4] "};
+    auto const x{parse(Utf8String(s))};
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal("\n"+readableRepr(e),R"--(
+Failed to parse array at line 1 column 2 because
+failed to parse "," then optional whitespace-separated list of element (recurse) commencing with "[" then optional whitespace and terminated by "]" then optional whitespace at line 1 column 2 because
+failed to parse "," at line 1 column 6 because
+line 1 column 6: expected ',' but found '4'.)--");
+  }
+  try{
+    auto const x{parse(Utf8String("1.06e10"))};
+    x->asArray();
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal("\n"+readableRepr(e),R"--(
+Failed to get 1.06e10 (at line 1 column 1) as an array because
+1.06e10 (at line 1 column 1) is not an Array.)--");
+  }
 }
 
 }
