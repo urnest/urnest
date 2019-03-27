4f86af84dfcbb826c3fddb20bbe124b7fb36031b
Trevor Taylor <urnest@onthenet.com.au>
2019-03-27 20:10:49 +1000

    xju/linux/wtmp refinements

--- xju/linux/wtmp/cat-wtmp.cc
+++ xju/linux/wtmp/cat-wtmp.cc
@@ -13,9 +13,17 @@
 #include <xju/format.hh>
 
 
+std::string t(std::chrono::system_clock::time_point const& t) noexcept
+{
+  return xju::format::localTime(
+    t,
+    xju::format::Year,"/",xju::format::Month,"/",xju::format::Day," ",
+    xju::format::Hour,":",xju::format::Minute,":",xju::format::Minute);
+}
+
 int main(int argc, char* argv[])
 {
-  if (argc!=1){
+  if (argc!=2){
     std::cerr << "usage: " << argv[0] << " <wtmp-file-name>\n";
     return 1;
   }
@@ -25,7 +33,17 @@ int main(int argc, char* argv[])
 
     auto const events{m.readEventsUntilDeadline(xju::steadyNow())};
     for (auto e: events){
-      //REVISIT
+      xju::linux::wtmp::Event::USER_LOGGED_IN const* ii;
+      xju::linux::wtmp::Event::USER_LOGGED_OUT const* oo;
+      if (ii=dynamic_cast<xju::linux::wtmp::Event::USER_LOGGED_IN const*>(&*e)){
+        auto const& i{ii->userLoggedIn_};
+        std::cout << t(i.at_) << ": " << i.user_ << " logged in from "
+                  << i.from_ << "\n";
+      }
+      else if (oo=dynamic_cast<xju::linux::wtmp::Event::USER_LOGGED_OUT const*>(&*e)){
+        auto const& o{oo->userLoggedOut_};
+        std::cout << t(o.at_) << ": " << o.who_.user_ << " logged out\n";
+      }
     };
     return 0;
   }
