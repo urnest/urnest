8bb5baa248ac496f0559bcd7eee8375db56e2ea6
Trevor Taylor <urnest@onthenet.com.au>
2020-02-25 20:43:43 +1000

    toward xju::http::requestLineParser()

--- xju/http/test-parseRequestLine.cc
+++ xju/http/test-parseRequestLine.cc
@@ -7,10 +7,9 @@
 // software for any purpose.  It is provided "as is" without express or
 // implied warranty.
 //
-#include <xju/http/requestLineParser.hh>
-
 #include <iostream>
 #include <xju/assert.hh>
+#include <xju/http/parseRequestLine.hh>
 
 namespace xju
 {
@@ -25,11 +24,31 @@ void test1() {
     xju::assert_equal(y.m_,Method("GET"));
     xju::assert_equal(y.t_,RequestTarget(
                         xju::uri::Path({
-                            xju::uri::Segment(),
+                            xju::uri::Segment(""),
                             xju::uri::Segment("x.html")}),
                         xju::uri::Query("")));
     xju::assert_equal(y.v_,HTTPVersion(HTTPVersion::Major(1),
                                        HTTPVersion::Minor(0)));
+  }
+  try
+  {
+    std::string const x("GET /x.html HTTP/1.0");
+    std::istringstream s(x);
+    RequestLine y(parseRequestLine(s,x.size()));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to parse HTTP request-line of max 20 chars from stream because\nfailed to read stream until \"\\r\\n\" reading at most 20 chars because\n20 chars read without seeing \"\\r\\n\" amongst \"GET /x.html HTTP/1.0\".");
+  }
+  try
+  {
+    std::string const x("GET /x.html http/1.0\r\n");
+    std::istringstream s(x);
+    RequestLine y(parseRequestLine(s,x.size()));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to parse HTTP request-line of max 22 chars from stream because\nfailed to parse RFC7230 (HTTP) request-line at line 1 column 1 because\nfailed to parse http-version at line 1 column 13 because\nfailed to parse \"HTTP/\" at line 1 column 13 because\nline 1 column 13: expected 'H' but found 'h'.");
   }
 }
 
