12657fd0f84e1040e4a6f73e227ce29633215bdb
Trevor Taylor <urnest@onthenet.com.au>
2020-03-02 20:21:06 +1000

    toward xju::http::parserHeaders

---
+++ xju/http/test-headerFieldParser.cc
@@ -0,0 +1,47 @@
+// Copyright (c) 2020 Trevor Taylor
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all.
+// Trevor Taylor makes no representations about the suitability of this
+// software for any purpose.  It is provided "as is" without express or
+// implied warranty.
+//
+#include <xju/http/headerFieldParser.hh>
+
+#include <iostream>
+#include <xju/assert.hh>
+
+namespace xju
+{
+namespace http
+{
+
+void test1() {
+  {
+    std::string const s("Host-Name: fred.com\r\n");
+    auto const r(hcp_parser::parseString(s.begin(),s.end(),
+                                         headerFieldParser()));
+    xju::assert_equal(
+      hcp_ast::reconstruct(hcp_ast::findOnlyChildOfType<HeaderFieldItem>(r)),
+      s);
+    xju::assert_equal(hcp_ast::findOnlyChildOfType<HeaderFieldItem>(r).get(),
+                      std::make_pair(FieldName("Host-Name"),
+                                     FieldValue("fred.com")));
+  }
+  @@@;
+}
+
+}
+}
+
+using namespace xju::http;
+
+int main(int argc, char* argv[])
+{
+  unsigned int n(0);
+  test1(), ++n;
+  std::cout << "PASS - " << n << " steps" << std::endl;
+  return 0;
+}
+
