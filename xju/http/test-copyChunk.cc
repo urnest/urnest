e4a215565a5064619ad99592c1bda036d58e8d4b
Trevor Taylor <urnest@onthenet.com.au>
2020-03-17 20:05:23 +1000

    xju::http::copyChunk

--- xju/http/test-copyChunk.cc
+++ xju/http/test-copyChunk.cc
@@ -58,10 +58,64 @@ void test1() {
   }
   
   // too big
-  REVISIT;
+  try
+  {
+    std::string const x(R"--(A)--" "\r\n"
+                        R"--(0123456789)--" "\r\n");
+    std::istringstream s(x);
+    Chunk const c(copyChunk(s,x.size()-1));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to copy HTTP chunk from specified std::istream assuming end of chunk occurs within 14 bytes because\n10 bytes of chunk data would exceed 14 total bytes.");
+  }
   // not enough data
+  try
+  {
+    std::string const x(R"--(30)--" "\r\n"
+                        R"--(0123456789)--" "\r\n");
+    std::istringstream s(x);
+    Chunk const c(copyChunk(s,1024));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to copy HTTP chunk from specified std::istream assuming end of chunk occurs within 1024 bytes because\nfailed to read 48 bytes of chunk-data then \\r\\n because\nread of next byte failed having read 12 bytes.");
+  }
+  try
+  {
+    std::string const x(R"--(B)--" "\r\n"
+                        R"--(0123456789)--" "\r\n");
+    std::istringstream s(x);
+    Chunk const c(copyChunk(s,1024));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to copy HTTP chunk from specified std::istream assuming end of chunk occurs within 1024 bytes because\nfailed to read 11 bytes of chunk-data then \\r\\n because\nfailed to read \\r\\n after data bytes because\nfailed to read stream until \"\\r\\n\" reading at most 2 chars because\nend of file before \"\\r\\n\" amongst \"\\n\".");
+  }
   // bad ext
+  try
+  {
+    std::string const x(R"--(A;name=")--" "\r\n"
+                        R"--(0123456789)--" "\r\n");
+    std::istringstream s(x);
+    Chunk const c(copyChunk(s,1024));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to copy HTTP chunk from specified std::istream assuming end of chunk occurs within 1024 bytes because\nfailed to parse HTTP Chunk Extension Value at line 1 column 8 because\nfailed to parse HTTP quoted-string at line 1 column 8 because\nfailed to parse \"\\\" at line 1 column 9 because\nline 1 column 9: expected '\\\\' but found '\\r'.");
+  }
   // bad size
+  try
+  {
+    std::string const x(R"--(9)--" "\r\n"
+                        R"--(0123456789)--" "\r\n");
+    std::istringstream s(x);
+    Chunk const c(copyChunk(s,x.size()));
+    xju::assert_never_reached();
+  }
+  catch(xju::Exception const& e){
+    xju::assert_equal(readableRepr(e),"Failed to copy HTTP chunk from specified std::istream assuming end of chunk occurs within 15 bytes because\nfailed to read 9 bytes of chunk-data then \\r\\n because\nfailed to read \\r\\n after data bytes because\nfailed to read stream until \"\\r\\n\" reading at most 2 chars because\n2 chars read without seeing \"\\r\\n\" amongst \"9\\r\".");
+  }
 }
 
 }
