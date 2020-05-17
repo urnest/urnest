4c0f585fb99e09644d10f91bd23fa3ef3dd16d06
Trevor Taylor <urnest@onthenet.com.au>
2020-05-17 19:47:09 +1000

    xju::ip::UDPDeliveryFailureNoticeQueue test pass including icmp failure (but not local failure)

--- xju/ip/test-UDPDeliveryFailureNoticeQueue.cc
+++ xju/ip/test-UDPDeliveryFailureNoticeQueue.cc
@@ -43,21 +43,20 @@ void test1() {
     xju::assert_equal(buffer,std::string("fred"));
   }
 }
-void test2(xju::HostName const& unreachableHost)
+void test2(xju::ip::v4::Address const& arpFailureHost)
 {
   // verify error sets POLLERR and does not try to collect pending
   // inbound messages; verify
-}
-
-void test3(xju::ip::v4::Address const& arpFailureHost)
-{
-  // verify non-existent local network host i.e. arp failure
-  // generates local delivery failure
+  // verify non-existent icmp failure
   // verify can send and receive without errors
   UDPSocket s1;
   auto const p1(s1.port());
   UDPDeliveryFailureNoticeQueue q1(s1);
 
+  UDPSocket s2;
+  UDPDeliveryFailureNoticeQueue q2(s2);
+
+  
   s1.sendTo({arpFailureHost,xju::ip::Port(22)},
             "fred",4,xju::steadyNow());
 
@@ -72,25 +71,27 @@ void test3(xju::ip::v4::Address const& arpFailureHost)
                       xju::io::PollInputState::ERR);
   }
 
-  //REVISIT: we get SyscallFailed(EHOSTUNREACH) rather than
-  //         getting the failure notice as successful read?
-  xju::assert_equal(q1.getFailureNotices(),
-                    {UDPDeliveryFailureNoticeQueue::DeliveryFailureNotice(
-                        {arpFailureHost,xju::ip::Port(22)},
-                        EHOSTUNREACH,
-                        0,
-                        std::tuple<
-                            xju::ip::v4::Address, // dropped by
-                            xju::ip::icmp::Message::Type, // why
-                            xju::ip::icmp::Message::Code  //  "
-                        >(xju::ip::v4::Address("127.0.0.1"),
-                          xju::ip::icmp::Message::Type::DEST_UNREACH,
-                          xju::ip::icmp::Message::Code(0)))});
+  s2.sendTo({xju::ip::v4::Address("127.0.0.1"),p1},
+            "fred",5U,xju::steadyNow());
+
+  {
+    auto const a(q1.getFailureNotices());
+    xju::assert_equal(a.size(),1U);
+    xju::assert_equal(a[0].to_.first,arpFailureHost);
+    xju::assert_equal(a[0].to_.second,xju::ip::Port(22));
+    xju::assert_equal(a[0].errno_,EHOSTUNREACH);
+    xju::assert_equal(a[0].droppedBecause_.valid(),true);
+    // dropped because could be anything
+    xju::assert_equal(std::get<1>(a[0].droppedBecause_.value()),
+                      xju::ip::icmp::Message::Type::DEST_UNREACH);
+    // code not predicted
+  }  
+
   
   xju::assert_equal(
     xju::io::poll({&s1},
                   {},
-                  xju::steadyNow()+std::chrono::seconds(10),
+                  xju::steadyNow(),
                   {&s1}),
     std::pair<std::map<xju::io::Input const*,xju::io::PollInputState>,
               std::map<xju::io::Output const*,xju::io::PollOutputState> >(
@@ -114,10 +115,7 @@ int main(int argc, char* argv[])
   unsigned int n(0);
   test1(), ++n;
   if(argc==2 && std::string(argv[1])!=""){
-    test2(xju::HostName(argv[1])), ++n;
-  }
-  if(argc==3 && std::string(argv[2])!=""){
-    test3(xju::ip::v4::Address(argv[2])), ++n;
+    test2(xju::ip::v4::Address(argv[1])), ++n;
   }
   std::cout << "PASS - " << n << " steps" << std::endl;
   return 0;
