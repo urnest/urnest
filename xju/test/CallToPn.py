73a9eb7ec64e199bca9482e3874d1d21825fc541
Trevor Taylor <urnest@onthenet.com.au>
2020-07-18 20:16:29 +1000

    cleanup python perms and interpreter statements since removing test code from pythong module files

--- xju/test/CallToPn.py
+++ xju/test/CallToPn.py
@@ -1,4 +1,20 @@
 #!/bin/env python
+# coding: utf-8
+#
+# Copyright (c) 2018 Trevor Taylor
+# 
+# Permission to use, copy, modify, and/or distribute this software for
+# any purpose with or without fee is hereby granted, provided that all
+# copyright notices and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 
 import sys
 
@@ -90,4 +106,4 @@ paramInits=''.join([',\n        p{n}_(p{n})'.format(**vars())
 paramMembers=''.join(['\n  P{n} const& p{n}_;'.format(**vars())
                       for n in range(1,nParams+1)])
 
-print template.format(**vars())
+print(template.format(**vars()))
