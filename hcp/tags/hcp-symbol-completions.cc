fdfc2ab70da03ae8f89e1b5f258b3bbbb63c35ab
Trevor Taylor <urnest@onthenet.com.au>
2020-07-20 20:45:02 +1000

    toward hcp tags symbol completions tool

--- hcp/tags/hcp-symbol-completions.cc
+++ hcp/tags/hcp-symbol-completions.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Trevor Taylor
+// Copyright (c) 2020 Trevor Taylor
 //
 // Permission to use, copy, modify, distribute and sell this software
 // and its documentation for any purpose is hereby granted without fee,
@@ -50,12 +50,12 @@ int main(int argc, char* argv[])
 {
   if (argc != 3){
     std::cerr << "usage: " << argv[0] << " <symbol> <from-scope>\n"
-              << "e.g:\n  " << argv[0] << " impl::Fred mod1::c::Mule\n"
-              << "... find definitions of impl::Fred in the context "
+              << "e.g:\n  " << argv[0] << " impl::Fre mod1::c::Mule\n"
+              << "... find completions of impl::Fre in the context "
               << "of mod1::c::Mule. Prints zero or more lines on stdout "
-              << "each containing <file> <line> of possible definition,e.g.:"
-              << "  /x/impl/Fred.hh 33\n"
-              << "  /y/mod1/impl/Fred 82\n"
+              << "each containing possible completion,e.g.:"
+              << "  impl::Fred\n"
+              << "  impl::Freda\n"
               << "... might be the output of the above example.\n"
               << "note: $TAG_LOOKUP_SERVICE_URL_FILE must locate url-file of tag-lookup-service to use - see tag-lookup-service" << std::endl;
     return 1;
@@ -68,13 +68,18 @@ int main(int argc, char* argv[])
     auto const ss(hcp::tags::splitSymbol(argv[1]));
     std::vector<hcp::tags::NamespaceName> fromScope(
       hcp::tags::splitScope(argv[2]));
-    auto const foundIn(
-      ref->lookupSymbol(fromScope,
-                        ss.first,
-                        ss.second));
-    for(auto const l: foundIn.locations_){
-      std::cout << xju::path::str(std::make_pair(l.directory,l.file))
-                << " " << l.line << std::endl;
+    auto const completions(
+      ref->lookupCompletions(fromScope,
+                             ss.first,
+                             ss.second));
+    for(auto c: completions){
+      std::vector<std::string> cc;
+      std::transform(c.scope_.begin(),c.scope_.end(),
+                     std::back_inserter(cc),
+                     xju::format::Str<decltype(c.scope_.front())>());
+      cc.push_back(xju::format::str(c.name_));
+      std::cout << xju::format::join(cc.begin(),cc.end(),
+                                     std::string("::")) << std::endl;
     }
     return 0;
   }
