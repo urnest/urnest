530dde53eb6c831d1fd1b760fe94fceaeb4f5704
Trevor Taylor <urnest@onthenet.com.au>
2020-07-30 20:29:48 +1000

    rework xju::Optional; move stringToFloat into xju namespace

--- xju/__init__.py
+++ xju/__init__.py
@@ -14,3 +14,39 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
+import subprocess
+import os
+from .xn import Xn,inContext,firstLineOf
+from typing import Tuple,Sequence
+
+class CmdFailed(Exception):
+    def __init__(self,argv,status,stderr):
+        self.argv=argv
+        self.status=status
+        self.stderr=stderr
+        Exception.__init__(self,'non-shell command {argv!r} failed with exit status {status} and stderr {stderr}'.format(**locals()))
+        pass
+    pass
+
+def doCmd(argv:Sequence[str])->Tuple[bytes,str]:
+    '''do non-shell command {argv!r}'''
+    '''returns (stdout,stderr) on zero exit status'''
+    '''raises CmdFailed on non-zero exit'''
+    try:
+        try:
+            p=subprocess.Popen(args=argv,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        except OSError as e:
+            if e.errno==2:
+                path=os.environ['PATH']
+                raise Exception('{argv[0]} not found on PATH {path}'.format(**locals())) from None
+            raise
+        out,err=p.communicate()
+        status=p.returncode
+        if status!=0:
+            raise CmdFailed(argv,status,err.decode('utf-8'))
+        return out,err.decode('utf-8')
+    except:
+        raise inContext(firstLineOf(doCmd.__doc__).format(**locals())) from None
+    pass
