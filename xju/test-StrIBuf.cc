647a41287536be751150a27134e73fb428fd0d8b
Trevor Taylor <urnest@onthenet.com.au>
2020-09-25 07:15:19 +1000

    add xju::StrIBuf

---
+++ xju/test-StrIBuf.cc
@@ -0,0 +1,47 @@
+// Copyright (c) 2020 Trevor Taylor
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all.
+// Trevor Taylor makes no representations about the suitability of this
+// software for any purpose.  It is provided "as is" without express or
+// implied warranty.
+//
+#include <xju/StrIBuf.hh>
+
+#include <iostream>
+#include <xju/assert.hh>
+
+namespace xju
+{
+
+void test1() {
+  StrIBuf x("onetwothree",3);
+  auto y(x.underflow());
+  xju::assert_equal(y.second-y.first,3U);
+  xju::assert_equal(std::string(y.first,y.second),"one");
+  y=x.underflow();
+  xju::assert_equal(y.second-y.first,3U);
+  xju::assert_equal(std::string(y.first,y.second),"two");
+  y=x.underflow();
+  xju::assert_equal(y.second-y.first,3U);
+  xju::assert_equal(std::string(y.first,y.second),"thr");
+  y=x.underflow();
+  xju::assert_equal(y.second-y.first,2U);
+  xju::assert_equal(std::string(y.first,y.second),"ee");
+  y=x.underflow();
+  xju::assert_equal(y.second-y.first,0U);
+}
+
+}
+
+using namespace xju;
+
+int main(int argc, char* argv[])
+{
+  unsigned int n(0);
+  test1(), ++n;
+  std::cout << "PASS - " << n << " steps" << std::endl;
+  return 0;
+}
+
