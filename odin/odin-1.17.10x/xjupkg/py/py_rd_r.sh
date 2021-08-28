dcb5f682c1f22c8683111781eda329685fd5b353
Trevor Taylor <urnest@onthenet.com.au>
2021-08-28 20:23:56 +1000

    py xjupkg odin package allow override main program directory

--- odin/odin-1.17.10x/xjupkg/py/py_rd_r.sh
+++ odin/odin-1.17.10x/xjupkg/py/py_rd_r.sh
@@ -3,6 +3,12 @@
 set -e
 
 ODIN_file="$1"
-ODIN_dirOfFile="$2"
+ODIN_py_r="$2"
+ODIN_dirOfFile="$3"
 
-echo "$ODIN_file+py_r=($ODIN_dirOfFile)" > py_rd_r
+if [ -n "$ODIN_py_r" ]
+then
+  echo "$ODIN_file+py_r=($ODIN_py_r)"
+else
+  echo "$ODIN_file+py_r=($ODIN_dirOfFile)"
+fi  > py_rd_r
