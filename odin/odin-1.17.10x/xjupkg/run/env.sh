3430cd802b5bc58eaab89960215dc98fffca65ac
Trevor Taylor <urnest@onthenet.com.au>
2021-09-03 07:43:06 +1000

    ignore header dependencies when buildin env.cc
    to reduce circular computations

--- odin/odin-1.17.10x/xjupkg/run/env.sh
+++ odin/odin-1.17.10x/xjupkg/run/env.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-env_exe=$1 && shift &&
+env_cc=$1 && shift &&
 ODIN_env=$1 && shift &&
 
 if [ -n "$ODINVERBOSE" ]
 then
   set -x
-fi
+fi &&
+
+env_exe=./env.exe &&
+
+(
+  export PATH=$ODIN_CXX_PATH &&
+  $ODIN_CXX $ODIN_CXX_FLAGS $env_cc -o $env_exe
+) &&
 
 cat "$ODIN_env" | "$env_exe" > env
 
