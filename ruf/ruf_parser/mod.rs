65e5874bc4582c2e7c247c5fb5f8abcd559feb5d
Trevor Taylor <urnest@onthenet.com.au>
2022-06-07 20:59:15 +1000

    toward ruf_parser

--- ruf/ruf_parser/mod.rs
+++ ruf/ruf_parser/mod.rs
@@ -34,6 +34,67 @@ pub struct ParseFailed<'text, 'goals, 'parser>
     pub context: Vec< Context<'text, 'goals> >    // outermost is last
 }
 
+#[derive(PartialEq)]
+pub struct LineCol
+{
+    pub line: i64,
+    pub col: i64
+}
+impl LineCol
+{
+    pub fn advance_through(&mut self, x: &str)
+    {
+	x.chars().for_each(|c|{
+	    match c {
+		'\n' => { self.line += 1; self.col = 1; },
+		'\r' => self.col = 1,
+		'\t' => self.col = (self.col/8+1)*8,
+		_c => { self.col += 1 },
+	    }
+	});
+    }
+}
+
+impl<'text, 'goals, 'parser> std::fmt::Display for ParseFailed<'text, 'goals, 'parser>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+	if f.alternate() {
+	    // multi-line format with preceding line, col phrase with inline line, col info, e.g.
+	    // failed to parse "fred" at line 3 column 5 because "jock..." does not start with "fred"
+	    let mut lc = LineCol { line: 1, col: 1};
+	    let mut prev_context = self.context.iter().rev().next();
+	    self.context.iter().rev().try_for_each(|context| {
+		let prev_text = prev_context.unwrap().at;
+		lc.advance_through(prev_text.split_at(prev_text.len()-context.at.len()).0);
+		prev_context = Some(context);
+		write!(f, "{line}:{col}: failed to {goal} because \n",
+		       goal = context.goal.to_string().as_str(),
+		       line = lc.line,
+		       col = lc.col)
+	    })?;
+	    write!(f, "{line}:{col}: {cause}", line = lc.line, col = lc.col, cause = self.cause)
+	}
+	else
+	{
+	    // single line human readable phrase with inline line, col info, e.g.
+	    // failed to parse "fred" at line 3 column 5 because "jock..." does not start with "fred"
+	    let mut lc = LineCol { line: 1, col: 1};
+	    let mut prev_context = self.context.iter().rev().next();
+	    self.context.iter().rev().try_for_each(|context| {
+		let prev_text = prev_context.unwrap().at;
+		lc.advance_through(prev_text.split_at(prev_text.len()-context.at.len()).0);
+		prev_context = Some(context);
+		write!(f, "failed to {goal} at line {line} column {col} because ",
+		       goal = context.goal.to_string().as_str(),
+		       line = lc.line,
+		       col = lc.col)
+	    })?;
+	    write!(f, "{}", self.cause)
+	}
+    }
+}
+
 pub type ParseResult<'text, 'goals, 'parser> = std::result::Result<
 	( crate::AST<'text>, &'text str ),
     ParseFailed<'text, 'goals, 'parser> >;
@@ -59,7 +120,7 @@ pub mod parse
 	    // REVISIT: need a rust-escape-string formatting function so that e.g.
 	    // Literal{ x: "the \"big\" bird" } formats as:
 	    // parse "the \"big\" bird"
-	    write!(f, "\"{}\"", self.x)
+	    write!(f, "parse \"{}\"", self.x)
 	}
     }
     pub struct LiteralNotFound<'cause> {
@@ -106,10 +167,9 @@ pub mod parse
 				      children: vec!() }, y) )
 		}
 		else {
-		    let goal = crate::Context{ goal: self, at: &text};
 		    crate::ParseResult::<'text, 'goals, 'parser>::Err(
 			crate::ParseFailed::<'text, 'goals, 'parser>{
-			    context: vec!( goal ),
+			    context: vec!(),
 			    cause: LiteralNotFound::new(&self.x, &text)
 			})
 		}
@@ -132,7 +192,7 @@ pub mod parse
     {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
 	{
-	    write!(f, "{}", self.tag)
+	    write!(f, "parse {}", self.tag)
 	}
     }
     impl<'content> crate::Parser for Tagged<'content>
