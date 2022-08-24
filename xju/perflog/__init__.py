4395568fde65d261e4cf5c54debd98bc50031e82
Trevor Taylor <urnest@onthenet.com.au>
2022-08-24 21:11:05 +1000

    draft perflog
    time sequence storage suiting "low" record rate e.g. 1 record per
    second and fetching by time range

--- xju/perflog/__init__.py
+++ xju/perflog/__init__.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2022 Trevor Taylor
+# coding: utf-8
+# 
+# Permission to use, copy, modify, and/or distribute this software for
+# any purpose with or without fee is hereby granted, provided that all
+# copyright notices and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# CMC - Context Manager Collections
+#
 
 
 from pathlib import Path
@@ -23,8 +40,9 @@ class PerfLog:
            - syncs file system before returning'''
         pass
 
-    def fetch(timestamp:from_, timestamp: to) -> Iter[Tuple[float, List[Union[str,int,float]]]]
+    def fetch(timestamp:from_, timestamp: to, max_records:int) -> Iter[Tuple[float, List[Union[str,int,float]]]]
         '''yield each record in range [from_, to) as a json encoded list of timestamp followed by column values in record (i.e. schema) order'''
+        count = 0
         for t in self.files[lower_bound(self.files.keys(), from_)..upper_bound(self.files.keys())];
             try:
                 with open(self.storage / self.relative_file_of(t), 'r') as f:
@@ -33,6 +51,9 @@ class PerfLog:
                         timestamp, col_values=decode_record(l[:-1], self.schema)
                         if timestamp >= from_ and timestamp < to:
                             yield (timestamp, col_values)
+                            count = count+1
+                            if count==max_records:
+                                return
                             pass
                         pass
                     pass
