bcc321810da65c0844ccef73c2923560eb014be8
Trevor Taylor <urnest@onthenet.com.au>
2023-02-20 20:51:23 +1000

    odin py package: fix python run script within source tree
    ... so it doesn't confuse import jsonschema with import xju.jsonschema

--- odin/xju-odin/pkg/py/py3.exec.sh
+++ odin/xju-odin/pkg/py/py3.exec.sh
@@ -44,7 +44,7 @@ fi &&
 L="" &&
 if [ "$ODINVERBOSE" != "" ]
 then
-  echo ${ODINRBSHOST}env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" \`cat "$ODIN_env"\` PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" python3 "$ODIN_FILE" $cmd; 
+  echo ${ODINRBSHOST}env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" \`cat "$ODIN_env"\` PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" python3 -P "$ODIN_FILE" $cmd; 
 fi &&
 (
   mkdir py3.exec &&
@@ -58,19 +58,19 @@ fi &&
     cd files &&
     if [ $ODIN_stderr = "trace" ]
     then
-      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 $cov "$ODIN_FILE" $cmd 2>&1 >../output
+      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 -P $cov "$ODIN_FILE" $cmd 2>&1 >../output
       echo $? > ../status
     elif  [ $ODIN_stderr = "output" ]
     then
-      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 $cov "$ODIN_FILE" $cmd >../output 2>&1
+      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 -P $cov "$ODIN_FILE" $cmd >../output 2>&1
       echo $? > ../status
     elif  [ $ODIN_stderr = "error" ]
     then
-      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 $cov "$ODIN_FILE"  $cmd >../output 2>../errors
+      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 -P $cov "$ODIN_FILE"  $cmd >../output 2>../errors
       echo $? > ../status
     elif [ $ODIN_stderr = "warn" ]
     then
-      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 $cov "$ODIN_FILE" $cmd >../output 2>../../WARNINGS
+      eval env - LD_LIBRARY_PATH="$ODIN_EXEC_LD_LIBRARY_PATH" PATH="$ODIN_EXEC_PATH" PYTHONPATH="$PYPATH:$ODIN_PYTHONPATH" `cat "$ODIN_env"` python3 -P $cov "$ODIN_FILE" $cmd >../output 2>../../WARNINGS
       echo $? > ../status
     else
       echo "error: +stderr, \"$ODIN_stderr\" is not one of trace, output, error, warn.">&2
