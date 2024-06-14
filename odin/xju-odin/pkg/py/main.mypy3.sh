93933b4263af75d55a4d30dc583f822b94d9ad1f
Trevor Taylor <urnest@onthenet.com.au>
2024-06-14 21:08:05 +1000

    xju-odin make per-options mypy cache to allow incremental
    mypy --incremental gives persistent incorrect results when
    flags/config/env changes, so make a per-flags/config/env cache dir
    work around mypy dumbidity

--- odin/xju-odin/pkg/py/main.mypy3.sh
+++ odin/xju-odin/pkg/py/main.mypy3.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
 
-ODIN_FILE=$1;shift;
+ODIN_FILE=$1;shift; 
 ODIN_CONFIG=$1;shift
 ODIN_env=$1;shift;
 pySp="$1";shift;
 ODIN_flags="$1";shift;
+ODIN_config_id=$(cat $1);shift;
+
 if [ -z "$pySp" ]
 then
     pySp=/dev/null
@@ -21,12 +23,15 @@ flags="$ODIN_MYPY_FLAGS"
       
 if [ "$ODIN_flags" != "" ] ; then flags="$flags `cat $ODIN_flags`"; fi
       
+cache_dir=$ODIN/mypy.cache.$ODIN_config_id
+mkdir -p "$cache_dir"
+
 L=""
 if [ "$ODINVERBOSE" != "" ] ; then
-   echo ${ODINRBSHOST}env - \`cat "$ODIN_env"\` LD_LIBRARY_PATH="$ODIN_MYPY_LD_LIBRARY_PATH" PATH="$ODIN_MYPY_PATH" PYTHONPATH="$ODIN_MYPY_PYTHONPATH" MYPYPATH="$PYPATH:$ODIN_MYPY_MYPYPATH" "$ODIN_MYPY" --cache-dir $ODIN --config-file $ODIN_CONFIG $flags "$ODIN_FILE"; 
+   echo ${ODINRBSHOST}env - \`cat "$ODIN_env"\` LD_LIBRARY_PATH="$ODIN_MYPY_LD_LIBRARY_PATH" PATH="$ODIN_MYPY_PATH" PYTHONPATH="$ODIN_MYPY_PYTHONPATH" MYPYPATH="$PYPATH:$ODIN_MYPY_MYPYPATH" "$ODIN_MYPY" --cache-dir "$cache_dir" --config-file $ODIN_CONFIG $flags "$ODIN_FILE"; 
  fi
 (
-  eval env - `cat "$ODIN_env"` LD_LIBRARY_PATH="$ODIN_MYPY_LD_LIBRARY_PATH" PATH="$ODIN_MYPY_PATH" PYTHONPATH="$ODIN_MYPY_PYTHONPATH" MYPYPATH="$PYPATH:$ODIN_MYPY_MYPYPATH" "$ODIN_MYPY" --cache-dir $ODIN --config-file $ODIN_CONFIG $flags $ODIN_FILE &&
+  eval env - `cat "$ODIN_env"` LD_LIBRARY_PATH="$ODIN_MYPY_LD_LIBRARY_PATH" PATH="$ODIN_MYPY_PATH" PYTHONPATH="$ODIN_MYPY_PYTHONPATH" MYPYPATH="$PYPATH:$ODIN_MYPY_MYPYPATH" "$ODIN_MYPY" --cache-dir "$cache_dir" --config-file $ODIN_CONFIG $flags $ODIN_FILE &&
   cat $ODIN_FILE > main.mypy3
 ) </dev/null 2>WARNINGS 1>&2 ||
 mv WARNINGS ERRORS
